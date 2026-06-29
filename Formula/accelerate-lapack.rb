9aeb6aa9310c3f348c4fc0af159064dacdb91315
Eric K Richardson <ekrichardson@gmail.com>
2026-06-29 01:23:19 +0000

    accelerate-lapack: add 2.0.0 bottle.

--- Formula/accelerate-lapack.rb
+++ Formula/accelerate-lapack.rb
@@ -5,6 +5,12 @@ class AccelerateLapack < Formula
   sha256 "8f1b47cf2ed255b22d5204218ec27466d4187b383b50daa5aeb55de4f0360cc5"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/ekrich/homebrew-tools/releases/download/accelerate-lapack-2.0.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "045917a8a787573f7fdd3738f0461783070c90a352144d69d46a3d2d1d7e0a5b"
+  end
+
   depends_on "cmake" => :build
   depends_on :macos
 
